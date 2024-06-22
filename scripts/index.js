db3c3c75116ea35c29e44c0e4aa65c3e0e1b1b10
DIFFFFFFT <672228275@qq.com>
2024-06-22 12:26:12 +0800

    change

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
 const { ipcRenderer } = require("electron");
 
+let blogList = []
+let blogListFinish = false
+let blogElementList = []
+
+ipcRenderer.addListener("back", () => {
+  history.back()
+})
+
 const isBlogDetail = (url) => {
   const pathRegex = /^https:\/\/medium\.com\/(.*)\/(.*)/;
   return pathRegex.test(url);
 };
+
+const isBlogList = (url) => {
+  const pathRegex1 = /^https:\/\/medium\.com\/?feed=(.*)/;
+  const pathRegex2 = /^https:\/\/medium\.com\/?tag=(.*)/;
+  return url === 'https://medium.com/' || pathRegex1.test(url) || pathRegex2;
+}
 
 const getBlogDetail = () => {
   const url = location.href;
@@ -39,14 +53,111 @@ const getBlogDetail = () => {
 };
 
 
-window.addEventListener("DOMContentLoaded", (event) => {
-  setInterval(() => {
-    const l = location.href;
-    if (isBlogDetail(l)) {
-      const blogDetail = getBlogDetail();
-      if (blogDetail) {
-        ipcRenderer.send("askAi", blogDetail);
+const firstLetterToLowerCase = (str) => {
+  if (str.length === 0) return str;  // 处理空字符串的情况
+  return str[0].toLowerCase() + str.slice(1);
+}
+
+const handleClickCategory = (category) => {
+  const lowerCategory = firstLetterToLowerCase(category)
+  const eles = document.querySelectorAll('#scroller-items button')
+  const url = `https://medium.com/?tag=${lowerCategory}`
+  if (url !== location.href) {
+    for (let i = 0; i < eles.length; i++) {
+      const ele = eles[i];
+      if (ele.innerText === category) {
+        ele.click()
+        break
       }
     }
+  }
+}
+
+const getBlogList = () => {
+  blogElementList = []
+  const main = document.querySelector('main')
+  const eles = main.querySelectorAll('a:has(h2)')
+  for (let i = 0; i < eles.length; i++) {
+    const ele = eles[i];
+    const regex = /^https:\/\/medium.com\/(.*\/.*)/;
+
+    // https://medium.com/@lukwagoasuman236/how-i-hacked-my-first-public-wi-fi-network-s-60e19dd19f14
+    // 域名/人名/文章名称/中划线/文章ID
+
+    const match = regex.exec(ele.href);
+    if (match) {
+      const id = match[1];
+      const url = "https://medium.com/" + id
+      // 装载完成
+      if (!blogListFinish) {
+        blogList.push({
+          url,
+          isClick: false,
+        })
+      }
+      ele.innerText = url
+      blogElementList.push(ele)
+    } else {
+      console.log("No match");
+    }
+  }
+
+  blogListFinish = true
+}
+
+const handleClickBlog = () => {
+  let needClickElement = null
+
+  for (let i = 0; i < blogList.length; i++) {
+    const blog = blogList[i];
+    if (!blog.isClick) {
+      // 至少有一个没点
+      blog.isClick = true
+      needClickElement = blogElementList.find(e => e.innerText === blog.url)
+      break
+    }
+  }
+
+  // 全部点击完成
+  if (!needClickElement) {
+    console.log('全部点击完成......');
+    // 清除数组
+    blogList = []
+    // 设置未装载完成
+    blogListFinish = false
+    // 刷新页面
+    location.reload()
+  } else {
+    console.log('点击文章......');
+    needClickElement.click()
+  }
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  let count = 0
+  setInterval(() => {
+    if(count >= 50){
+      location.href =  "https://medium.com/"
+      location.reload()
+      return
+    }
+    const url = location.href;
+    if (isBlogDetail(url)) {
+      const blogDetail = getBlogDetail();
+      if (blogDetail) {
+        console.log('获取数据完成，......');
+        ipcRenderer.send("saveAndAskAi", blogDetail);
+        count = 0
+      }
+    } else if (isBlogList(url)) {
+      handleClickCategory('Business')
+      getBlogList();
+      handleClickBlog()
+      count = 0
+    } else {
+      history.back()
+      count = 0
+    }
+    count++
   }, 3000);
 });
--- service/ArticleService.js
+++ service/ArticleService.js
@@ -1,19 +1,92 @@
 const Article = require("../model/Article");
+const { ipcMain } = require("electron");
 
 class ArticleService {
   constructor() {
-    // this.articles = [];
   }
 
-  // 获取所有文章
-  // getAllArticles() {
-  //     return this.articles;
-  // }
+  static async saveAndAskAi(blogDetail, webContents) {
+    const article = await Article.findOne({
+      where: {
+        url: blogDetail.url
+      }
+    })
 
-  // 添加文章
-  static async addArticle(article) {
-    await Article.create(article);
-  }
+    if (article && article.isAnwser) {
+      console.log("AI ing or end............");
+      return
+    } else {
+      await Article.create({
+        url: blogDetail.url,
+        title: blogDetail.postTitle,
+        subTitle: blogDetail.subtitleParagraph,
+        time: blogDetail.publishDate,
+        content: blogDetail.content,
+        isAnwser: 1,
+        anwser: ''
+      });
+    }
+
+    console.log("AI ing............");
+
+    // 询问这篇文章是做什么的，具有什么商业价值，中文描述一下
+    fetch("http://localhost:11434/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "qwen2",
+        messages: [
+          {
+            role: "user",
+            content: "文章JSON信息:" + JSON.stringify(blogDetail),
+          },
+          {
+            role: "user",
+            content: `
+            你现在是个非常穷的穷人，但是你能通过阅读，学习，思考，总结，来改变自己的命运，实现财务自由。
+            你需要思考并回答以下内容:
+
+            1.该文章是否具有商业价值。
+            2.该文章是否具有创业方案，能否落地出产品与服务。
+            3.确定你的产品或服务是否满足市场的需求，以及你的竞争优势是什么。
+            4.你的商业模式应该清晰明确，包括收入来源、成本结构、利润预期等。这决定了你的企业如何盈利。
+            5.明确你的目标客户是谁，他们的需求是什么，以及你如何能够最好地满足这些需求。
+            6.如何拥有一个强大的团队至关重要。确定你的合作伙伴和员工，确保他们有能力和动力共同追求企业的目标。
+            7.评估启动资金的需求，包括初期投资和运营资金。确定如何融资，并考虑长期的财务健康。
+            8.了解和遵守相关的法律、规章和行业标准。这包括注册公司、知识产权保护、税务义务等。
+            9.制定一个有效的市场营销计划，包括品牌建设、推广和销售渠道的选择。
+            10.根据业务需求，评估并选择合适的技术和基础设施支持，确保业务运作的顺畅和效率。
+            11.识别潜在的风险，并制定应对策略。这包括市场风险、财务风险、操作风险等方面。
+            12.考虑企业的长远发展，制定可持续发展策略，包括扩展计划和创新战略。这些是创业过程中的一些关键考虑因素，每个因素都需要深入思考和具体规划，以提高创业成功的可能性。
+            `
+          },
+        ],
+        stream: false,
+      }),
+    })
+      .then((res) => {
+        res.json().then((data) => {
+          console.log("AI end............");
+          const content = data.choices[0].message.content;
+          Article.update({
+            anwser: content
+          }, {
+            where: {
+              url: blogDetail.url
+            }
+          });
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        console.log("back......");
+        webContents.send('back')
+      });
+  };
 }
 
 module.exports = ArticleService
